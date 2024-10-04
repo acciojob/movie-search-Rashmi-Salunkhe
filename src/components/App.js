1bddb370bb0d28c7c25f60c10aecb962804b6a27
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-04 10:06:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
-import React from "react";
-import './../styles/App.css';
+// src/App.js
+import React from 'react';
+import MovieSearch from './MovieSearch';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <h1>Movie Search</h1>
+      <MovieSearch />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
